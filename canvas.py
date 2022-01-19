b0a10488cebe1658b9ae0725cddba67f1b89d5e9
tj-schultz <73593696+tj-schultz@users.noreply.github.com>
2022-01-18 23:13:58 -0500

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -7,16 +7,19 @@ date: 1/14/22
 
 import tkinter as tk
 import path as pth
+import time
 
 ## canvas class
 class Canvas(tk.Frame):
-    down = False
+    down = True
+    path = pth.Path(pth.Point(0, 0))
     def __init__(self, parent, *args, **kwargs):
 
         tk.Frame.__init__(self, parent, *args, **kwargs)
 
         self.parent = parent
 
+    ## returns pointer position
     def get_pointer_pos(self, event):
         print(event.x, event.y)
         return (event.x, event.y)
@@ -25,15 +28,21 @@ class Canvas(tk.Frame):
     def toggle_down(self):
         self.down = not self.down
 
-    ## pen method to draw path
+    ## pen tool method to draw path
     def pen(self, event):
         self.toggle_down()
         start_x = event.x
         start_y = event.y
-        path = pth.Path(pth.Point(start_x, start_y))
-        while (self.down):
-            
-            path.append(pth.Point())
-            print(path)
+        self.path = pth.Path(pth.Point(start_x, start_y))
 
+    ## log motion using stitch
+    def log_motion(self, x, y):
+        time.sleep(0.1)
+        if self.down:
+            self.path.stitch(pth.Point(x, y))
 
+    def draw_polyline(self, event):
+        canvas.log_motion(event.x, event.y)
+        if len(canvas.path.parsed_path > 2):
+            canvas.create_line(canvas.path.parsed_path[-2].x, canvas.path.parsed_path[-2].y, \
+                               canvas.path.parsed_path[-1].x, canvas.path.parsed_path[-1].y)
