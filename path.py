90e747dba3c1efbca8147e4981483835f2ffe4ac
tj-schultz <73593696+tj-schultz@users.noreply.github.com>
2022-01-18 23:13:54 -0500

    Update path.py

--- path.py
+++ path.py
@@ -17,12 +17,19 @@ class Point():
 
 ## path class
 class Path():
-    parsed_path = [256]
-    head = None
+    parsed_path = []
 
     def __init__(self, p):
         head = p
-        self.parsed_path[-1] = p
+        self.parsed_path.append(p)
 
-    def append(self, p):
-        self.parsed_path[-1] = p
+    def stitch(self, p):
+        self.parsed_path.append(p)
+
+    def get_length(self):
+        return len(self.parsed_path)
+
+    def display(self):
+        print('Length:%s\tPath:\n' % len(self.parsed_path))
+        for i in range(len(self.parsed_path)):
+            print('%s, %s\n' % (self.parsed_path[i].x, self.parsed_path[i].y))
