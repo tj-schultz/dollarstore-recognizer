0de1c01a14084fe64b1a88eaccdf9456dcd138d5
tj-schultz <73593696+tj-schultz@users.noreply.github.com>
2022-01-25 14:31:04 -0500

    Create recognizer.py

---
+++ recognizer.py
@@ -0,0 +1,18 @@
+"""
+name: recognizer.py -- dollarstore-recognizer
+description: Recognizer class with member functions to resample, rotate, scale/translate paths and
+run calculations to determine the score for a particular recognizer
+authors: TJ Schultz, []
+date: 1/25/22
+"""
+import path
+import dollar
+
+## recognizer class containing canvas display methods for
+class Recognizer():
+    def __init__(self, parent, _pcanvas, WIDTH, HEIGHT):
+
+        ## input path canvas
+        self.canvas = _pcanvas
+
+if __name__ == '__main__':
