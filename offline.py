f1fc3f5712e1c159618ae7cd1a58cf7e17b858b7
tj-schultz <t.schultz@ufl.edu>
2022-02-01 13:07:08 -0500

    Create offline.py

---
+++ offline.py
@@ -0,0 +1,12 @@
+"""
+name: offline.py -- dollarstore-recognizer
+description: alternative launching script to perform a recognition loop
+and log results offline
+authors: TJ Schultz, Skylar McCain
+date: 2/01/22
+"""
+
+import xml
+import csv
+
+## read a single xml file gesture
