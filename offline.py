53906fd83aac5d26ab4f3c6e3d35d35a8e848181
skylarmccain <skylarmccain@ufl.edu>
2022-02-09 18:58:27 -0500

    Update offline.py
    added functionality to implement random100 with recognizer
    need to add output to csv implementation

--- offline.py
+++ offline.py
@@ -13,6 +13,8 @@ import os
 import path as pth
 import recognizer as rec
 import dollar
+import random
+import pandas as pd
 
 ## list of path types to read using filenames with '01-10' appended
 xml_filetypes = ["arrow", "caret", "check", "circle", "delete_mark", "left_curly_brace",\
@@ -50,6 +52,37 @@ def read_XML_path(filepath):
 
     return xml_path
 
+def random100_test(R):
+
+    # dictionary to store random list of templates each repetion of random100, size = 16 * e
+    templates = {}
+    # to store one randomly selected canidate for each gesture, size = 16
+    canidates = {}
+    for user in R.preprocessed:
+        for e in range(1,2):
+            for i in range(1,2):
+                for gesture in R.preprocessed[user]:
+                    canidates[gesture] = {}
+                    for temp in random.sample(R.preprocessed[user][gesture].keys(), e + 1):
+                        id = "_".join([gesture,temp])
+                        templates[id] = R.preprocessed[user][gesture][temp]
+                    canidates[gesture][id] = templates.pop(id)
+                #print("\nTemplates\n", list(templates))
+                #print("\nCandiates:\n", canidates)
+                for gesture in canidates.keys():
+                    for canidate in canidates[gesture]:
+                        #print(canidate)
+                        #call recognizer on canidate with list of randomly generated templates from above
+                        print(user, gesture, canidate, "scores\n", R.recognize(canidates[gesture][canidate], templates), "\n")
+                        #if correct
+                            #score++
+                        #write score/100 to output-doc at user, gesture, level e
+                        #write [user, gesture, e, e*16, templates, x, result of recognize]
+              
+                templates.clear()
+                canidates.clear()
+
+
 if __name__ == "__main__":
 
     ## build xml_base
@@ -70,6 +103,7 @@ if __name__ == "__main__":
 
     ## instantiate the recognizer and preprocess the template dictionary recursively
     R = rec.Recognizer(xml_base)
+    random100_test()
 
     ## debug -- vectors should be of length 2 * 64 = 128
     for user in R.preprocessed:
